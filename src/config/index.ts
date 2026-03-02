fcb5c4f8b1d075e26c0fd4a28acdd929396a6510
kvanshu504-design <kvanshu504@gmail.com>
2026-03-02 12:24:58 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -28,15 +28,14 @@ export const SITE_CONTENT: SiteContent = {
     name: "Vanshika Kataria",
     specialty: "UI & Product Designer",
     summary:
-      "Designer based in Bangalore, India. I specialize in Web and Mobile UI applications, Brand Logo & Identity and Product Design System.",
+      "I’m Vanshika Kataria, a UI Designer based in Bangalore with roots in Punjab. I craft intuitive, visually engaging interfaces with a strong user-first approach. Passionate about creating meaningful digital experiences through thoughtful design",
     email: "vanshikakataria42@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "TruKKer Technologies",
+      position: "Designer-1",
+      startDate: "Dec 2024 - Current",
       summary: [
         "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
         "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
@@ -44,31 +43,22 @@ export const SITE_CONTENT: SiteContent = {
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
+      company: "Cybernext Pvt. Ltd",
+      position: "Jr. UI/UX Designer",
+      startDate: "Nov 2023",
+      endDate: "Oct 2024",
       summary: [
         "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
         "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
       ],
     },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
-    },
-  ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
+      name: "Premium Watch Marketplace",
+      summary: "The Luxury of UI is a conceptual UI/UX design project for a premium watch marketplace, focused on translating the elegance, exclusivity, and craftsmanship of luxury timepieces into a refined digital experience",
       linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      linkSource: "https://www.behance.net/gallery/234799843/The-Luxury-of-UI-Designing-a-Premium-Watch-Marketplace",
+      image: "",
     },
     {
       name: "Shopp App",
